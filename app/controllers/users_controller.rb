33e7be62a5df18b6715a8967239fce17ac9df141
Nick Batchelder <nbatch1995@yahoo.com>
2015-10-22 00:56:28 +1000

    redirect to login for new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,8 +33,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        format.html { redirect_to login_path, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: login_path }
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
